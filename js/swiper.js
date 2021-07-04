9026e36637df32f3e5a33e88b9b86b8965b4a599
SilverMoon17 <50795723+SilverMoon17@users.noreply.github.com>
2021-07-04 20:23:50 +0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1.15,
+    centeredSlides: true,
+    loop: true,
+    mousewheel: true,
+    // cssMode: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
